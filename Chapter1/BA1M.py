65de95f5544e9ca9dbcaaf557554bc8b7f859199
NikolinaBritvic <100932879+NikolinaBritvic@users.noreply.github.com>
2022-08-03 09:50:29 +0200

    Update BA1M.py

--- Chapter1/BA1M.py
+++ Chapter1/BA1M.py
@@ -1,25 +1,13 @@
 # Implement NumberToPattern
 
-def NumberToSymbol(s):
-  if (s==0):
-    return "A"
-  elif (s==1):
-    return "C"
-  elif (s==2):
-    return "G"
-  else:
-    return "T"
-    
-    
-def NumberToPattern(index,k):
-  if(k==1):
-    return NumberToSymbol(index)
-  q=index//4
-  r=index%4
-  s=NumberToSymbol(r)
-  prefixPattern=NumberToPattern(q,k-1)
-  return  prefixPattern+s
+def NumberToPattern(number,k):
+  rez=""
+  D={0:"A",1:"C",2:"G",3:"T"}
+  for i in range(k):
+    rez+=str(D[number%4])
+    number=number//4
+  return rez
 
-index=int(input("Index: "))
+number=int(input("Number: "))
 k=int(input("k: "))
-print(NumberToPattern(index,k))
+print(NumberToPattern(number,k))
